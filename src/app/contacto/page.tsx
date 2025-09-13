1c01b934b03de54204626c6cbdedc253b421ecd7
Mayo-YB21 <yahirbllmayogbh@gmail.com>
2025-09-13 16:42:34 -0600

    paginas creadas

---
+++ src/app/contacto/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react' //El React es como una estructura inicial para que funcione el codigo, despues puedes modificarlo a tu gusto.
+const page = () => {
+  return (
+    <>
+    <h1>Pagina de Contacto</h1>
+    </>
+  )
+}
+
+export default page
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (     ///Este  <> </> es un Framento el cual sirve para evitar errores y manejar todo lo que tiene que ver con el html o el return
     <>   
-    <h1>Sitio web de demostración</h1>
+    <h1>Tienda Productos Fifa 2026 </h1>
     </> 
   );
 }
